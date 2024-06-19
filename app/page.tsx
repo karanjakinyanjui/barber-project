cb4e06a874842ff9a3ac4defa2f8bbb63a4a430e
Kinyanjui Karanja <ekkinyanjui@gmail.com>
2024-06-19 14:41:08 +0300

    show only unclaimed tx on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,33 @@
-import { getAdmin, getUser } from "@/auth";
+import { getDBUser, getUser } from "@/auth";
 import TransactionList from "@/components/transactions/TransactionList";
 import prisma from "@/prisma/client";
 import React from "react";
 import { TransactionsTable } from "./transactions/_components/TransactionsTable";
 
 const TransactionsPage = async () => {
-  const user = await getUser();
-  // console.log(user);
-
-  const admin = await getAdmin();
-
+  const user = await getDBUser();
+  const isAdmin = user?.role === "ADMIN";
   if (!user) return null;
 
+  const where = {
+    where: {},
+  };
+  if (!isAdmin) {
+    where.where = {
+      userId: null,
+    };
+  }
+
   const transactions = await prisma.transaction.findMany({
-    // where: {
-    //   userId: null,
-    // },
+    ...where,
     include: {
       User: true,
     },
   });
 
-  console.log(transactions);
-
   return (
     <div className="p-1 md:p-4">
-      {admin ? (
+      {isAdmin ? (
         <TransactionsTable transactions={transactions} offset={null} />
       ) : (
         <TransactionList transactions={transactions} />
